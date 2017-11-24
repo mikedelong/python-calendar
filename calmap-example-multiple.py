514f7691231b79873816e3812e467c457c9c5a07
Mike DeLong <mikedelong@outlook.com>
2017-11-24 13:13:06 -0500

    cleanup: optimized imports.

--- calmap-example-multiple.py
+++ calmap-example-multiple.py
@@ -1,10 +1,11 @@
 # https://pythonhosted.org/calmap/
 
-import numpy as np; np.random.seed(sum(map(ord, 'calmap')))
-import pandas as pd
 import calmap
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
+np.random.seed(sum(map(ord, 'calmap')))
 
 all_days = pd.date_range('1/15/2014', periods=700, freq='D')
 days = np.random.choice(all_days, 500)
