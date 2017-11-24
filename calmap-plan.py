0a7683f2a0a6ec83efae84e272443c4b35333d3a
Mike DeLong <mikedelong@outlook.com>
2017-11-24 14:45:02 -0500

    feature: Changed to use a list constructor instead of individual holidays; also added a similar block of code for travel days, which should probably not be counted against data preparation days, but currently are.

--- calmap-plan.py
+++ calmap-plan.py
@@ -1,4 +1,3 @@
-
 import calmap
 import matplotlib.pyplot as plt
 import numpy as np
@@ -21,9 +20,23 @@ if random_days:
     days = np.random.choice(all_days, 50)
 else:
     days = all_days.copy()
-    # todo remove any holidays here
-    mlk_day = datetime.date(year=2018, month=1, day=15)
-    holidays = [mlk_day]
+    travel_days = [
+        datetime.date(year=2017, month=11, day=28),
+        datetime.date(year=2017, month=11, day=29),
+        datetime.date(year=2017, month=11, day=30),
+        datetime.date(year=2017, month=12, day=1),
+        datetime.date(year=2017, month=12, day=1),
+        datetime.date(year=2017, month=12, day=4),
+        datetime.date(year=2017, month=12, day=5),
+        datetime.date(year=2017, month=12, day=6)
+    ]
+    days = days.drop(travel_days)
+    holidays = [
+        datetime.date(year=2017, month=12, day=25),
+        datetime.date(year=2018, month=1, day=1),
+        datetime.date(year=2018, month=1, day=15),
+        datetime.date(year=2018, month=2, day=19)
+    ]
     days = days.drop(holidays)
 
 seventy = int(0.7 * len(days))
@@ -34,7 +47,8 @@ random_events = False
 if random_events:
     events = pd.Series(np.random.randn(len(days)), index=days)
 else:
-    values = [10 if index > seventy else 1 for index in range(0, len(days))]
+    values = [1 if index < seventy else 2 if index < eighty else 3 if index < ninety else 4 for index in
+              range(0, len(days))]
     events = pd.Series(values, index=days)
 
 calmap.calendarplot(events,
