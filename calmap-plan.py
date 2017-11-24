d8e848fcf7e1f6f749138ddf0e49324d5cc46fad
Mike DeLong <mikedelong@outlook.com>
2017-11-24 13:27:56 -0500

    feature: switched from days to business days, fixed the day labels.

--- calmap-plan.py
+++ calmap-plan.py
@@ -7,19 +7,20 @@ import datetime
 
 start_date = datetime.datetime.today()
 end_date = datetime.datetime(year=2018, month=2, day=28)
-all_days = pd.date_range(start=start_date, end=end_date, freq='D')
+all_days = pd.date_range(start=start_date, end=end_date, freq='B')
 
 days = np.random.choice(all_days, 50)
+
 events = pd.Series(np.random.randn(len(days)), index=days)
 
 calmap.calendarplot(events,
-                    monthticks=3,
-                    daylabels='MTWTFSS',
-                    # dayticks=[0, 2, 4, 6],
                     cmap='YlGn',
-                    # fillcolor='grey',
+                    daylabels='MTWTFSS',
+                    fig_kws=dict(figsize=(8, 4)),
                     linewidth=0,
-                    fig_kws=dict(figsize=(8, 4))
+                    monthticks=3,
+                    # dayticks=[0, 2, 4, 6],
+                    # fillcolor='grey',
                     )
 
 plt.show()
