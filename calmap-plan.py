f5013669c5710a91b6864ff2ba58090c85b29d27
Mike DeLong <mikedelong@outlook.com>
2017-11-24 13:19:57 -0500

    feature: reduced the intensity of the gray for no data, added back the previous missing days of the week.

--- calmap-plan.py
+++ calmap-plan.py
@@ -12,9 +12,14 @@ all_days = pd.date_range(start=start_date, end=end_date, freq='D')
 days = np.random.choice(all_days, 50)
 events = pd.Series(np.random.randn(len(days)), index=days)
 
-calmap.calendarplot(events, monthticks=3, daylabels='MTWTFSS',
-                    dayticks=[0, 2, 4, 6], cmap='YlGn',
-                    fillcolor='grey', linewidth=0,
-                    fig_kws=dict(figsize=(8, 4)))
+calmap.calendarplot(events,
+                    monthticks=3,
+                    daylabels='MTWTFSS',
+                    # dayticks=[0, 2, 4, 6],
+                    cmap='YlGn',
+                    # fillcolor='grey',
+                    linewidth=0,
+                    fig_kws=dict(figsize=(8, 4))
+                    )
 
 plt.show()
